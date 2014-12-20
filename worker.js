3cac2f8c4fc3eeeaba8a3e2d6198551b15dd0e60
John Zila <john@jzila.com>
2014-12-19 16:04:58 -0800

    Added write authentication

--- worker.js
+++ worker.js
@@ -3,5 +3,12 @@ var Transformer = require("./transformer.js");
 
 var hnRef = new Firebase("https://hacker-news.firebaseio.com/v0/");
 var transformerRef = new Firebase("https://brilliant-torch-9244.firebaseio.com/hnbot/");
+var firebaseSecret = process.env.FIREBASE_SECRET;
+transformerRef.auth(firebaseSecret, function(err) {
+    if (err === null) {
+        new Transformer(hnRef, transformerRef);
+    } else {
+        console.log("Failed to authenticate with Firebase.");
+    }
+});
 
-new Transformer(hnRef, transformerRef);
