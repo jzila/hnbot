de0c54ab57178acf247347d416a36f2fbeb2e824
John Zila <john@jzila.com>
2014-12-30 12:23:17 -0800

    Removed call to deprecated firebase auth()

--- worker.js
+++ worker.js
@@ -4,7 +4,7 @@ var Transformer = require("./transformer.js");
 var hnRef = new Firebase("https://hacker-news.firebaseio.com/v0/");
 var transformerRef = new Firebase("https://brilliant-torch-9244.firebaseio.com/hnbot/");
 var firebaseSecret = process.env.FIREBASE_SECRET;
-transformerRef.auth(firebaseSecret, function(err) {
+transformerRef.authWithCustomToken(firebaseSecret, function(err) {
     if (err === null) {
         console.log("Authenticated. Starting transformer.");
         new Transformer(hnRef, transformerRef);
